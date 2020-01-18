ca15adae1142ecbb565516655c6a342e8ced26fd
plavc <>
2020-01-18 14:26:48 +0100

    docker compose remove duplicated configs param, cleanup ru.sh

--- run.sh
+++ run.sh
@@ -1,32 +1,38 @@
 #!/usr/bin/env bash
+#
+# Run helper script
 
-DOCKER_STACK_NAME="rabbitmq"
+set -euo pipefail
+IFS=$'\n\t'
 
-deploy() {
-    docker stack deploy -c docker-compose.yml $DOCKER_STACK_NAME
+readonly DOCKER_STACK_NAME="rabbitmq"
+readonly DOCKER_COMPOSE="docker-compose.yml"
+readonly REDEPLOY_SLEEP=15
+
+_deploy() {
+    docker stack deploy -c "${DOCKER_COMPOSE}" "${DOCKER_STACK_NAME}"
 }
 
-rm() {
-    docker stack rm $DOCKER_STACK_NAME
+_rm() {
+    docker stack rm "${DOCKER_STACK_NAME}"
 }
 
-if [ $1 = "deploy" ]; then
-    deploy
-fi
+_redeploy() {
+  _rm && sleep ${REDEPLOY_SLEEP} && _deploy
+}
 
-if [ $1 = "rm" ]; then
-    rm
-fi
+_help_short() {
+  echo "Usage: $0 { deploy | redeploy | rm }"
+}
 
-if [ $1 = "redeploy" ]; then
-    rm
+main() {
+  local command="${1:-}"
+  case ${command} in
+    deploy)   _deploy ;;
+    redeploy) _redeploy ;;
+    rm)       _rm ;;
+    *)        _help_short "$@" ;;
+  esac
+}
 
-    echo -n "Wait 15 seconds for Docker to release all resources"
-    for i in {1..15}
-    do
-        echo -n "."
-        sleep 1
-    done
-    echo ""
-    deploy
-fi
+main "$@"
